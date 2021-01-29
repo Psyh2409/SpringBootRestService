a95bd367c4f8b67bb5304cb9ec328639f2c1dfca
Psyh2409 <psyh2409@gmail.com.com>
2021-01-29 14:55:01 +0200

    initial commit

---
+++ src/main/java/com/gmail/psyh2409/SBRSApp.java
@@ -0,0 +1,15 @@
+package com.gmail.psyh2409;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+https://habr.com/ru/post/435144/
+topjava 4 января 2019 в 19:53
+Введение в Spring Boot: создание простого REST API на Java
+* */
+@SpringBootApplication
+public class SBRSApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SBRSApp.class);
+    }
+}
